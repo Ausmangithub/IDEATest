a63ffe4294f7e080e00e06148064e86340602c8e
Ausman <19937380419@163.com>
2019-07-27 18:32:15 +0800

    第2次提交测试代码

--- src/com/mashiyuan/test/GitTest.java
+++ src/com/mashiyuan/test/GitTest.java
@@ -3,5 +3,6 @@ package com.mashiyuan.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试代码");
+        System.out.println("第二次测试代码");
     }
 }
