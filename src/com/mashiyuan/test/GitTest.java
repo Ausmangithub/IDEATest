740d9ed265b5e3cf476ff9889a7177d9bf46150b
Ausman <19937380419@163.com>
2019-07-27 19:07:32 +0800

    冲突测试

--- src/com/mashiyuan/test/GitTest.java
+++ src/com/mashiyuan/test/GitTest.java
@@ -3,6 +3,7 @@ package com.mashiyuan.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试代码");
-        System.out.println("第二次测试代码");
+        System.out.println("冲突测试");
+
     }
 }
